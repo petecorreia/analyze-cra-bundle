5d90c7daf8a5af9e5e5710dab270405441e3a812
Pete Correia <pete@petecorreia.com>
2018-12-11 16:02:36 +0000

    Added simple analysis of gzipped bundle size

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,64 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command';
+import * as fs from 'fs';
+import * as gzipSize from 'gzip-size';
+import * as glob from 'glob';
 
 class AnalyzeCraBundle extends Command {
-  static description = 'describe the command here'
+	static description = 'Analyzes your CRA bundle';
 
-  static flags = {
-    // add --version flag to show CLI version
-    version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+	static flags = {
+		version: flags.version({ char: 'v' }),
+		help: flags.help({ char: 'h' }),
+		output: flags.string({ char: 'o', default: 'build-analysis.json' }),
+	};
 
-  static args = [{name: 'file'}]
+	static args = [{ name: 'pathToBuildFolder', default: 'build' }];
 
-  async run() {
-    const {args, flags} = this.parse(AnalyzeCraBundle)
+	async run() {
+		const {
+			args: { pathToBuildFolder },
+			flags: { output = 'build-analysis.json' },
+		} = this.parse(AnalyzeCraBundle);
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
-  }
+		if (!pathToBuildFolder) {
+			this.error('No path to build folder specified specified. e.g. analyze-cra-bundle ./build');
+		}
+
+		let history: number[];
+
+		try {
+			const previousAnalysis = JSON.parse(fs.readFileSync(output, 'utf8'));
+			history = previousAnalysis.history || [0];
+		} catch {
+			history = [0];
+		}
+
+		const files = glob.sync(pathToBuildFolder + '/static/js/*.js');
+
+		if (!files || !files.length) {
+			this.error('No JS files found in build/static/js');
+		}
+
+		const size = files.reduce((t, f) => (t += (gzipSize as any).fileSync(f)), 0);
+		const previousSize = history[history.length - 1];
+		const increase = size - previousSize;
+
+		this.log(formatBytes(size));
+		this.log(formatBytes(increase));
+
+		fs.writeFileSync(output, JSON.stringify({ size, increase }, null, 2), 'utf8');
+
+		this.exit();
+	}
 }
 
-export = AnalyzeCraBundle
+function formatBytes(bytes: number, decimals?: number): string {
+	if (bytes === 0) return '0 Bytes';
+	const k = 1024;
+	const dm = !decimals || decimals <= 0 ? 0 : decimals || 2;
+	const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+	const i = Math.floor(Math.log(bytes) / Math.log(k));
+	return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+export = AnalyzeCraBundle;
