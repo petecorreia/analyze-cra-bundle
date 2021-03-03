fe11a17e16526ded5d8b3455ffb6e68fb92c13b6
Pete Correia <pete@petecorreia.com>
2021-03-03 18:28:09 +0000

    Tweaks to dev config

--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,83 @@
-import { Command, flags } from '@oclif/command';
-import * as fs from 'fs';
-import * as gzipSize from 'gzip-size';
-import * as execa from 'execa';
-import chalk from 'chalk';
-import { setStatus, generateSpaces, formatBytes } from './utils';
+import { Command, flags } from '@oclif/command'
+import * as fs from 'fs'
+import * as gzipSize from 'gzip-size'
+import * as execa from 'execa'
+import chalk from 'chalk'
+import { setStatus, generateSpaces, formatBytes } from './utils'
 
 class AnalyzeCraBundle extends Command {
-	static description = 'Analyzes your CRA bundle';
+	static description = 'Analyzes your CRA bundle'
 
 	static flags = {
 		version: flags.version({ char: 'v' }),
 		help: flags.help({ char: 'h' }),
 		buildFolderPath: flags.string({ char: 'f', default: 'build' }),
-	};
+	}
 
 	async run() {
 		const {
 			flags: { buildFolderPath = 'build' },
-		} = this.parse(AnalyzeCraBundle);
+		} = this.parse(AnalyzeCraBundle)
 
-		this.log();
-		this.log(chalk.magentaBright('Analyzing bundle... '));
+		this.log()
+		this.log(chalk.magentaBright('Analyzing bundle... '))
 
-		let indexFile;
+		let indexFile
 
 		try {
-			indexFile = fs.readFileSync(buildFolderPath + '/index.html', 'utf8');
+			indexFile = fs.readFileSync(buildFolderPath + '/index.html', 'utf8')
 		} catch {
-			this.log();
+			this.log()
 			this.error(
-				`Build folder '${buildFolderPath}' does not exist.\n\nMaybe tweak your build path?\nanalyze-cra-bundle path/to/build/folder`
-			);
-			return;
+				`Build folder '${buildFolderPath}' does not exist.\n\nMaybe tweak your build path?\nanalyze-cra-bundle -f path/to/build/folder`
+			)
+			return
 		}
 
-		const fileMatches = indexFile.match(/src="\/static\/js.*?\.js"/gi);
-		const files = (fileMatches && fileMatches.map(match => match.replace(/src="(.*)"$/, '$1'))) || [];
+		const fileMatches = indexFile.match(/src="\/static\/js.*?\.js"/gi)
+		const files = (fileMatches && fileMatches.map((match) => match.replace(/src="(.*)"$/, '$1'))) || []
 
 		if (!files || !files.length) {
-			this.log();
+			this.log()
 			this.error(
 				'No JS files found in build/static/js.\n\nMaybe tweak your build path?\nanalyze-cra-bundle path/to/build/folder'
-			);
+			)
 		}
 
-		this.log();
-		this.log(chalk.magentaBright('index.html:'));
+		this.log()
+		this.log(chalk.magentaBright('index.html:'))
 
 		const size = files.reduce((t, f) => {
 			// @ts-ignore - gzipSize is missing fileSync in its type but it exists
-			const gzippedSize = gzipSize.fileSync(buildFolderPath + f);
-			const spaces = generateSpaces(Math.max(35 - f.length, 2));
-			const filename = f.replace('/static/js/', '');
-			this.log(
-				`${chalk.gray('/static/js/')}${chalk.magentaBright(filename)}${spaces}${formatBytes(gzippedSize)}`
-			);
-			return t + gzippedSize;
-		}, 0);
+			const gzippedSize = gzipSize.fileSync(buildFolderPath + f)
+			const spaces = generateSpaces(Math.max(35 - f.length, 2))
+			const filename = f.replace('/static/js/', '')
+			this.log(`${chalk.gray('/static/js/')}${chalk.magentaBright(filename)}${spaces}${formatBytes(gzippedSize)}`)
+			return t + gzippedSize
+		}, 0)
 
-		this.log();
-		this.log(`${chalk.bold(formatBytes(size))}`);
+		this.log()
+		this.log(`${chalk.bold(formatBytes(size))}`)
 
 		if (process.env.CI && process.env.TRAVIS) {
-			this.log();
-			this.log('Adding Github status check...');
+			this.log()
+			this.log('Adding Github status check...')
 			await setStatus({
 				context: 'metrics/bundle-size',
 				description: formatBytes(size),
 				env: process.env as any,
 				command: this,
-			});
-			this.log();
-			this.log('Github status check added!');
+			})
+			this.log()
+			this.log('Github status check added!')
 		}
 
-		this.log();
-		this.log(chalk.magentaBright(chalk.bold.magentaBright('Bundle analysis completed! 🎉')));
-		this.log();
+		this.log()
+		this.log(chalk.magentaBright(chalk.bold.magentaBright('Bundle analysis completed! 🎉')))
+		this.log()
 
-		this.exit();
+		this.exit()
 	}
 }
 
-export = AnalyzeCraBundle;
+export = AnalyzeCraBundle
--- src/utils.ts
+++ src/utils.ts
@@ -1,25 +1,15 @@
-import Command from '@oclif/command';
-import { ReposCreateStatusParams } from '@octokit/rest';
+import Command from '@oclif/command'
+import { ReposCreateStatusParams } from '@octokit/rest'
+const { octokit } = require('@octokit/rest')()
 
-// tslint:disable-next-line
-const octokit = require('@octokit/rest')();
-
-type Status = {
-	sha: string;
-	target_url: string;
-	owner: string;
-	repo: string;
-	githubToken: string;
-};
-
-type EventType = 'push' | 'pull_request' | 'api' | 'cron';
+type EventType = 'push' | 'pull_request' | 'api' | 'cron'
 
 type Env = {
-	TRAVIS_EVENT_TYPE: EventType;
-	TRAVIS_REPO_SLUG: string;
-	TRAVIS_JOB_WEB_URL: string;
-	ANALYZE_CRA_GITHUB_TOKEN: string;
-};
+	TRAVIS_EVENT_TYPE: EventType
+	TRAVIS_REPO_SLUG: string
+	TRAVIS_JOB_WEB_URL: string
+	ANALYZE_CRA_GITHUB_TOKEN: string
+}
 
 export async function setStatus({
 	context,
@@ -27,21 +17,21 @@ export async function setStatus({
 	env,
 	command,
 }: {
-	context: string;
-	description: string;
-	env: Env;
-	command: Command;
+	context: string
+	description: string
+	env: Env
+	command: Command
 }) {
-	const { TRAVIS_EVENT_TYPE, TRAVIS_REPO_SLUG, TRAVIS_JOB_WEB_URL, ANALYZE_CRA_GITHUB_TOKEN } = env;
+	const { TRAVIS_EVENT_TYPE, TRAVIS_REPO_SLUG, TRAVIS_JOB_WEB_URL, ANALYZE_CRA_GITHUB_TOKEN } = env
 
-	const sha = getCommitSha(TRAVIS_EVENT_TYPE, command);
+	const sha = getCommitSha(TRAVIS_EVENT_TYPE, command)
 
-	if (!sha) return;
+	if (!sha) return
 
-	const repoSlug = TRAVIS_REPO_SLUG;
-	const parsedSlug = repoSlug.split('/');
-	const owner = parsedSlug[0];
-	const repo = parsedSlug[1];
+	const repoSlug = TRAVIS_REPO_SLUG
+	const parsedSlug = repoSlug.split('/')
+	const owner = parsedSlug[0]
+	const repo = parsedSlug[1]
 
 	const params: ReposCreateStatusParams = {
 		owner,
@@ -51,41 +41,41 @@ export async function setStatus({
 		target_url: TRAVIS_JOB_WEB_URL,
 		description,
 		context,
-	};
+	}
 
 	octokit.authenticate({
 		type: 'oauth',
 		token: ANALYZE_CRA_GITHUB_TOKEN,
-	});
+	})
 
-	await octokit.repos.createStatus(params);
+	await octokit.repos.createStatus(params)
 }
 
 function getCommitSha(eventType: EventType, command: Command) {
 	if (eventType === 'push') {
-		return process.env.TRAVIS_COMMIT;
+		return process.env.TRAVIS_COMMIT
 	} else if (eventType === 'pull_request') {
-		const travisCommitRange = process.env.TRAVIS_COMMIT_RANGE;
+		const travisCommitRange = process.env.TRAVIS_COMMIT_RANGE
 		if (!travisCommitRange) {
-			command.error('TRAVIS_COMMIT_RANGE not found');
-			return;
+			command.error('TRAVIS_COMMIT_RANGE not found')
+			return
 		}
-		const parsed = travisCommitRange.split('...');
-		return parsed.length === 1 ? travisCommitRange : parsed[1];
+		const parsed = travisCommitRange.split('...')
+		return parsed.length === 1 ? travisCommitRange : parsed[1]
 	}
 
-	command.error(`event type '${eventType}' not supported`);
-	return;
+	command.error(`event type '${eventType}' not supported`)
+	return
 }
 
 export function formatBytes(bytes: number, decimals = 2): string {
-	if (bytes === 0) return '0 Bytes';
-	const k = 1024;
-	const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-	const i = Math.floor(Math.log(bytes) / Math.log(k));
-	return parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)) + ' ' + sizes[i];
+	if (bytes === 0) return '0 Bytes'
+	const k = 1024
+	const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+	const i = Math.floor(Math.log(bytes) / Math.log(k))
+	return parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)) + ' ' + sizes[i]
 }
 
 export function generateSpaces(howMany: number) {
-	return Array.from({ length: howMany }).reduce((acc, s) => acc + ' ', '');
+	return Array.from({ length: howMany }).reduce((acc, s) => acc + ' ', '')
 }
